78312cc79b869c1425f1a1bbc0a91454e7d6981b
= <=>
2024-01-31 14:44:56 -0800

    hh

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Basic Dictionalry """
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCache):
+    """ No limit Caching system inherited from BaseCache """
+    def put(self, key, item):
+        """ Assign to dict """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Return linked value """
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        return self.cache_data.get(key)
